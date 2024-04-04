4368155fcea7a5949f41c09bb1b273de82e524ad
lolpng944 <71849379+lolpng944@users.noreply.github.com>
2024-04-05 01:25:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -260,3 +260,11 @@ setInterval(() => {
     sendBatchedMessages(room);
   });
 }, server_tick_rate);
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
