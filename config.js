6656a569cfa924f09d986e16199c546dfd265678
lolpng944 <71849379+lolpng944@users.noreply.github.com>
2024-05-25 19:10:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const SHOOT_COOLDOWN = 100;
 const BULLET_DAMAGE = 5;
 const game_start_time = 5000;
 const game_win_rest_time = 10000;
-const max_room_players = 2;
+const max_room_players = 4;
 const maxClients = 100;
 
 const playerHitboxWidth = 60; 
