ccbd3f45148616e917855653b733f7fb9838e843
lolpng944 <71849379+lolpng944@users.noreply.github.com>
2024-06-19 16:59:18 +0200

    Update bullets.js

--- bullets.js
+++ bullets.js
@@ -4,44 +4,46 @@ const { weaponCooldowns, weaponShootRange } = require('./config');
 
 function handleBulletFired(result, player) {
   const room = result.room;
-  //const player = room.players.get(result.playerId);
 
-  // const currentTime = new Date().getTime();
-  // const lastShootTime = player.lastShootTime || 0;
+  const currentTime = new Date().getTime();
+  const lastShootTime = player.lastShootTime || 0;
+  const shootCooldown = weaponCooldowns[player.gun];
 
-  if (player.shooting === false) {
-    player.shooting = true;
-
-    const finalshootdirection = player.shoot_direction - 90;
-    const radiansfinal = (finalshootdirection * Math.PI) / 180;
-    const bulletLength = weaponShootRange[player.gun];
-    const bulletEndpoint = calculateBulletEndpoint(
-      player.x,
-      player.y,
-      radiansfinal,
-      bulletLength,
-    );
- 
-    const bullet = {
-      startX: player.x,
-      startY: player.y,
-      endX: bulletEndpoint.x,
-      endY: bulletEndpoint.y, 
-      direction: radiansfinal,
-      playerId: result.playerId,
-      gun: player.gun,
-      // width: 50,
-    };
-
-    handleBulletCollision(room, bullet);
-
-    const shootCooldown = weaponCooldowns[player.gun];
-
-    setTimeout(() => {
-      player.shooting = false;
-    }, shootCooldown); //
+  // Ensure the player can't switch weapons while shooting
+  if (player.shooting || (currentTime - lastShootTime < shootCooldown)) {
+    return;
   }
+
+  player.shooting = true;
+  player.lastShootTime = currentTime;
+
+  const finalshootdirection = player.shoot_direction - 90;
+  const radiansfinal = (finalshootdirection * Math.PI) / 180;
+  const bulletLength = weaponShootRange[player.gun];
+  const bulletEndpoint = calculateBulletEndpoint(
+    player.x,
+    player.y,
+    radiansfinal,
+    bulletLength,
+  );
+
+  const bullet = {
+    startX: player.x,
+    startY: player.y,
+    endX: bulletEndpoint.x,
+    endY: bulletEndpoint.y, 
+    direction: radiansfinal,
+    playerId: result.playerId,
+    gun: player.gun,
+  };
+
+  handleBulletCollision(room, bullet);
+
+  setTimeout(() => {
+    player.shooting = false;
+  }, shootCooldown);
 }
+
 
 
 module.exports = {
