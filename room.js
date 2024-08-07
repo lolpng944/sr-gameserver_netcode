7b5f5c25b91ad32b136bf07b0fd4e75fd3da7782
turbowarp lover <71849379+lolpng944@users.noreply.github.com>
2024-08-07 15:45:34 +0200

    Update room.js

--- room.js
+++ room.js
@@ -325,7 +325,7 @@ function sendBatchedMessages(roomId) {
     // ...(room.lastSent?.sendping !== room.sendping ? { pg: room.sendping } : {}),
     rp: room.players.size,
     id: room.state === "playing" ? undefined : room.map,
-    ...(room.eliminatedPlayers && room.eliminatedPlayers.length > 0 ? { ep: room.eliminatedPlayers } : {}),
+    ...(room.eliminatedPlayers && room.eliminatedPlayers.length > 0 && room.lastSent?.ep !== room.eliminatedPlayers ? { ep: room.eliminatedPlayers } : {}),
   };
 
   //pl: room.state === "playing" ? room.lastSent?.maxplayers !== room.maxplayers ? { pl: room.maxplayers } : {} : room.maxplayers,
