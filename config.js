373c130c89e589d7d9db2314787a1982653b0fe5
turbowarp lover <71849379+lolpng944@users.noreply.github.com>
2024-08-07 03:23:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const batchedMessages = new Map();
 const rooms = new Map();
 
-const server_tick_rate = 20;
+const server_tick_rate = 18;
 //const matchmaking_timeout = 100000;
 const matchmaking_timeout = 300000;
 const player_idle_timeout = 60000;
