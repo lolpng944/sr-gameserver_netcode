33e748ddb57e7eba83417b106fcf7df41526e1ad
turbowarp lover <71849379+lolpng944@users.noreply.github.com>
2024-08-09 04:36:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -73,11 +73,11 @@ const gunsconfig = {
     damage: 2,
     useplayerangle: true,
     bullets: [
-      { angle: 0, speed: 25, distance: 300, delay: 0, offset: -10 },
-      { angle: 0, speed: 25, distance: 300, delay: 100, offset: 10 },
-      { angle: 0, speed: 25, distance: 300, delay: 200, offset: -10 },
-      { angle: 0, speed: 25, distance: 300, delay: 300, offset: 10 },
-      { angle: 0, speed: 25, distance: 300, delay: 400, offset: -10 }
+      { angle: 0, speed: 35, distance: 350, delay: 0, offset: 0 },
+     // { angle: 0, speed: 25, distance: 300, delay: 100, offset: 10 },
+     // { angle: 0, speed: 25, distance: 300, delay: 200, offset: -10 },
+      //{ angle: 0, speed: 25, distance: 300, delay: 300, offset: 10 },
+     // { angle: 0, speed: 25, distance: 300, delay: 400, offset: -10 }
 
     ]
   },
