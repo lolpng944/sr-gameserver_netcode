1bbcce6e06d06ffb7dcd5ff99fb349ce1e7999c1
turbowarp lover <71849379+lolpng944@users.noreply.github.com>
2024-08-09 16:01:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const matchmaking_timeout = 300000;
 const player_idle_timeout = 60000;
 const WORLD_WIDTH = 800;
 const WORLD_HEIGHT = 800;
-const playerspeed = 0.40;
+const playerspeed = 0.28;
 const SHOOT_COOLDOWN = 100; 
 const BULLET_DAMAGE = 5;
 const game_start_time = 5000;
@@ -32,7 +32,7 @@ const gamemodeconfig = {
   1: {
     maxplayers: 1,
     respawns_allowed: 0,
-    zonespeed: 13.4,
+    zonespeed: 1.4,
     usezone: true,
     health_restore: true,
     health_autodamage: true,
